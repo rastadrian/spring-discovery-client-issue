2010a6622d1bd65305a2a720917c0c71de18b71b
Luis Adrian Pena Alaniz <adrian.pena@azeti.net>
2022-04-13 12:39:57 +0200

    Initial commit

---
+++ src/main/java/com/rastadrian/k8s/clientissue/ClientIssueApplication.java
@@ -0,0 +1,48 @@
+package com.rastadrian.k8s.clientissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ClientIssueApplication {
+
+    @RestController
+    public static class ClientController {
+
+        private final ReactiveDiscoveryClient discoveryClient;
+
+        public ClientController(ReactiveDiscoveryClient discoveryClient) {
+            this.discoveryClient = discoveryClient;
+        }
+
+        @GetMapping("/services")
+        public Flux<String> getServices() {
+            return discoveryClient.getServices();
+        }
+    }
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClient() {
+        return WebClient
+                .builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientIssueApplication.class, args);
+    }
+
+}
